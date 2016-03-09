6892a64ca00257d075b6fd9449344a91a66f502a
Salman Shan <salman.shan@phaedrasolutions.com>
2016-03-09 21:27:35 +0500

    setup done

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+	as_enum :role, executive: 0, manager: 1, director: 2, hr: 3, admin: 4        
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create :email => "admin@buzzrapid.com", :password => "password", :role_cd => 4
+User.create :email => "hr@buzzrapid.com", :password => "password", :role_cd => 3
+User.create :email => "director@buzzrapid.com", :password => "password", :role_cd => 2
+User.create :email => "manager@buzzrapid.com", :password => "password", :role_cd => 1
+User.create :email => "executive@buzzrapid.com", :password => "password", :role_cd => 0
